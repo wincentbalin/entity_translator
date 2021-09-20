d28bf491acbc48d5b38a7b48a39b8c6a7aa7ab1c
Wincent Balin <wincent.balin@gmail.com>
2021-09-20 20:47:29 +0200

    Added man function and IndexedDB guard

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
-// Load manifest file and process it
-fetch('data/manifest.min.json', {
-    'headers': {
-        'If-Modified-Since': 'Tue, 25 Aug 2021 20:29:00 GMT'
+(function() {
+    if (!window.indexedDB) {
+        console.error('IndexedDB is not supported!');
+        return;
     }
-})
-    .then(function(response) {
-        switch (response.status) {
-            case 200:
-                console.log('Process manifest...')
-                break;
 
-            case 304:
-                console.log('Manifest is cached, do nothing');
-                break;
-
-            default:
-                console.error('Server error');
-                break;
+    // Load manifest file and process it
+    fetch('data/manifest.min.json', {
+        'headers': {
+            'If-Modified-Since': 'Tue, 25 Aug 2021 20:29:00 GMT'
         }
     })
-    .catch(function(error) {
-        console.error(error);
-    });
+        .then(function(response) {
+            switch (response.status) {
+                case 200:
+                    console.log('Process manifest...')
+                    break;
+
+                case 304:
+                    console.log('Manifest is cached, do nothing');
+                    break;
+
+                default:
+                    console.error('Server error');
+                    break;
+            }
+        })
+        .catch(function(error) {
+            console.error(error);
+        });
+
+})();
