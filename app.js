1da3a7c23bcce091e1396e0028d1c8ed5e172cf1
Wincent Balin <wincent.balin@gmail.com>
2021-11-19 17:34:23 +0100

    Added fetch-based download function with progress

--- app.js
+++ app.js
@@ -1,4 +1,51 @@
 (function() {
+    var downloadFile = function(url) {
+        return new Promise(async function(resolve, reject) {
+            /*
+            * Large part of this function was taken from https://javascript.info/fetch-progress
+            */
+            let response = await fetch(url);
+            const reader = response.body.getReader();
+
+            const contentLength = +response.headers.get('Content-Length');
+
+            let progress = document.querySelector('#progress');
+            progress.value = 0;
+            progress.max = contentLength;
+
+            let receivedLength = 0;
+            let chunks = [];
+
+            while (true) {
+                const {done, value} = await reader.read();
+
+                if (done) {
+                    break;
+                }
+
+                chunks.push(value);
+                receivedLength += value.length;
+                progress.value = receivedLength;
+            }
+
+            let chunksAll = new Uint8Array(receivedLength);
+            let position = 0;
+            chunks.forEach(function(chunk) {
+                chunksAll.set(chunk, position);
+                position += chunk.length;
+            });
+
+            let result = new TextDecoder('utf-8').decode(chunksAll);
+            resolve(result);
+        });
+    }
+
+    downloadFile('data/manifest.min.json').then(function(result) {
+        console.log(result.length);
+        var json = JSON.parse(result);
+        console.log(json);
+    });
+
     if (!window.indexedDB) {
         console.error('IndexedDB is not supported!');
         return;
